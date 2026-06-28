617c233b5a72b1e615c34b2adfba7c9b0b718e4e
Gustas Mikelionis <mikelionisgustas@gmail.com>
2026-06-28 22:59:21 +0100

    (Hopefully) working image euphorics generation algorithm

--- GemmaComparisonWrapper.py
+++ GemmaComparisonWrapper.py
@@ -11,18 +11,6 @@ class GemmaComparisonWrapper:
         self.device = device
         self.processor = self.load_processor()
         self.model = self.load_model(self.device)
-        self.apply_monkey_patches()
-
-    def apply_monkey_patches(self):
-
-        # Workaround for torch.no_grad annotation on generate method
-        unwrapped_generate = self.model.generate.__wrapped__
-
-        def generate_with_grad(*args, **kwargs):
-            with torch.enable_grad():
-                return unwrapped_generate(self.model, *args, **kwargs)
-
-        self.model.generate = generate_with_grad
 
     def load_processor(self):
         return AutoProcessor.from_pretrained(self.MODEL_ID)
@@ -45,7 +33,7 @@ class GemmaComparisonWrapper:
         messages.append({"role": "user", "content": comparison_prompt_content})
         return messages
 
-    def prepare_inference(self, prompt, enable_thinking):
+    def prepare_inference(self, prompt, enable_thinking=True):
         inputs = self.processor.apply_chat_template(
             prompt,
             tokenize=True,
@@ -67,22 +55,7 @@ class GemmaComparisonWrapper:
     def decode_logit(self, logit):
         return self.processor.tokenizer.decode(logit)
 
-    def decode_logits(self, logits):
-        s = ""
-        for l in logits:
-            s += self.decode_logit(torch.argmax(l[0]))
-        return s
-
-    def get_preference_logits(self, logits, num_choices, shuffled_image_dict):
-        log_probs = torch.zeros(num_choices)
-        choice_logits = logits[-2][0]
-        for i in range(num_choices):
-            choice = i + 1
-            original_idx = shuffled_image_dict[i]['original_idx']
-            log_probs[original_idx] = choice_logits[self.processor.tokenizer.vocab[f'{choice}']]
-        return log_softmax(log_probs)
-
-    def get_preference_logits_forward(self, next_token_logits, num_choices, shuffled_image_dict):
+    def get_preference_logits(self, next_token_logits, num_choices, shuffled_image_dict):
         log_probs = torch.zeros(num_choices)
         for i in range(num_choices):
             choice = i + 1
@@ -90,10 +63,10 @@ class GemmaComparisonWrapper:
             log_probs[original_idx] = next_token_logits[self.processor.tokenizer.vocab[f'{choice}']]
         return log_softmax(log_probs)
 
-    def compare_using_forward(self, images, comparison_question, enable_thinking=True):
+    def compare_and_find_preferred_image(self, images, comparison_question):
         shuffled_image_dict = shuffle_image_dict(images)
         prompt = self.construct_comparison_prompt(shuffled_image_dict, comparison_question)
-        inputs, generation_config, input_len = self.prepare_inference(prompt, enable_thinking)
+        inputs, generation_config, input_len = self.prepare_inference(prompt, enable_thinking=False)
         outputs = self.model(
             input_ids=inputs["input_ids"],
             pixel_values=inputs["pixel_values"],
@@ -103,9 +76,8 @@ class GemmaComparisonWrapper:
             return_dict=True,
         )
 
-        logits = outputs.logits
         next_token_logits = outputs.logits[0, -1, :]
-        preference_logits = self.get_preference_logits_forward(next_token_logits, len(images), shuffled_image_dict)
+        preference_logits = self.get_preference_logits(next_token_logits, len(images), shuffled_image_dict)
         predicted_token = self.processor.tokenizer.decode(torch.argmax(next_token_logits))
         if not predicted_token.isdigit():
             return -1
@@ -113,24 +85,6 @@ class GemmaComparisonWrapper:
         if preferred_image < 0 or preferred_image >= len(images):
             return -1
         return shuffled_image_dict[preferred_image]['original_idx'], preference_logits, next_token_logits
-
-    def compare_and_find_preferred_image(self, images, comparison_question, enable_thinking=True):
-        shuffled_image_dict = shuffle_image_dict(images)
-        prompt = self.construct_comparison_prompt(shuffled_image_dict, comparison_question)
-        inputs, generation_config, input_len = self.prepare_inference(prompt, enable_thinking)
-        outputs = self.model.generate(**inputs, generation_config=generation_config)
-
-        logits = outputs.logits
-        response = self.decode_logits(logits)
-        preference_logits = self.get_preference_logits(logits, len(images), shuffled_image_dict)
-
-        # response = self.processor.decode(outputs[0][input_len:], skip_special_tokens=False)
-        parsed_response = self.processor.parse_response(response)
-        if enable_thinking:
-            print(parsed_response['thinking'])
-        content = parsed_response["content"]
-        preferred_image = int(content) - 1
-        return shuffled_image_dict[preferred_image]['original_idx'], preference_logits, logits[-2][0]
 
     def prompt_image_description(self, image):
         prompt = [
